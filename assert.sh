80db44d6df63907e89c2c9a8d19694668dc36dab
Christian Lascsak <christian.lascsak7@gmail.com>
2022-01-03 21:12:52 +0100

    add improvements to sequential version

--- assert.sh
+++ assert.sh
@@ -1,4 +1,4 @@
 #usr/bin/bash
-echo "Asserting Serial version..."
+echo "Asserting Sequential version..."
 difference="$(diff sequential_matrix.txt matrix_original.txt)"
 [[ ! -z "$difference" ]] && echo "NOK" || echo "OK"
--- sequential_version.cpp
+++ sequential_version.cpp
@@ -6,36 +6,22 @@ const int m = 100, n = 100, p = 3;
 float A[m][n][p];
 
 int main() {
-	//loop to fill in every row
 	for (int i = 0; i < m; i++) {
-		//loop to fill in every column
 		for (int j = 0; j < n; j++) {
-			//loop to fill in each 2D matrix
-			for (int k = 0; k < p; k++) {
-				//for matrix k=0
-				if (k == 0) {
-					A[i][j][k] = (float)i / ((float)j + 1.00);
-				}
-				//for matrix k=1
-				else if (k == 1) {
-					A[i][j][k] = 1.00;
-				}
-				//for matrix k=2
-				else {
-					A[i][j][k] = (float)j / ((float)i + 1.00);
-				}
-			}
+			A[i][j][0] = (float)i / ((float)j + 1.00);
+			A[i][j][1] = 1.00;
+			A[i][j][2] = (float)j / ((float)i + 1.00);
 		}
 	}
 
 	//Iteration count
 	for (int t = 0; t < 24; t++) {
-		//each row - beware first row and last row not to be updated therefore from 1...8190
+		//each row - beware first row and last row not to be updated therefore from 1...m-1
 		for (int i = 1; i < m - 1; i++) {
 			//each column
 			for (int j = 0; j < n; j++) {
 				//only matrix k=1 is updated
-				A[i][j][1] = A[i][j][1] + (1 / (sqrt(A[i + 1][j][0] + A[i - 1][j][2])));
+				A[i][j][1] += 1 / sqrt(A[i + 1][j][0] + A[i - 1][j][2]);
 			}
 		}
 	}
