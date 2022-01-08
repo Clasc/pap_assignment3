8ad4048ce48aa88e010f2729fe2cc522ca3976eb
Christian Lascsak <christian.lascsak7@gmail.com>
2022-01-08 14:23:55 +0100

    add some boilerplate loading code

--- src/parallel_version.hpp
+++ src/parallel_version.hpp
@@ -1,6 +1,7 @@
 #define CL_TARGET_OPENCL_VERSION 200
-#include <CL/cl.h>
-#include <stdio.h>
+#include<CL/cl.hpp>
+#include<stdio.h>
+#include<vector>
 
 void run_parallel() {
     cl_int err;
@@ -13,4 +14,22 @@ void run_parallel() {
     }
 
     printf("\nDetected OpenCL platforms: %d \n", numPlatforms);
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    auto platform = platforms.front();
+
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    auto device = devices.front();
+
+    auto cl_code = readClFile("parallel.cl");
+    cl_context context;
+    std::vector<cl_device_type> device;
+    getDevices
+}
+
+// returns the opencl code in a specific file as a string
+std::string readClFile(std::string filename) {
+    std::ifstream file(filename);
+    return std::string src(std::istreambuf_iterator<char>(file), (std::istreambuf_iterator<char>()));
 }
