11e8bf5d1fe6f2f18cc96b734f3eeb409fa4d0e8
Christian Lascsak <christian.lascsak7@gmail.com>
2022-01-10 23:27:20 +0100

    make compilable and runnable first version

--- src/parallel.cl
+++ src/parallel.cl
@@ -1,20 +1,16 @@
-#if defined(cl_khr_fp64)
-#  pragma OPENCL EXTENSION cl_khr_fp64: enable
-#elif defined(cl_amd_fp64)
-#  pragma OPENCL EXTENSION cl_amd_fp64: enable
-#else
-#  error double precision is not supported
-#endif
-
-kernel void add(
-       ulong n,
-       global const double *a,
-       global const double *b,
-       global double *c
-       )
-{
-    size_t i = get_global_id(0);
-    if (i < n) {
-       c[i] = a[i] + b[i];
-    }
+__kernel void hello_world(__global char *string) {
+  string[0] = 'H';
+  string[1] = 'e';
+  string[2] = 'l';
+  string[3] = 'l';
+  string[4] = 'o';
+  string[5] = ',';
+  string[6] = ' ';
+  string[7] = 'W';
+  string[8] = 'o';
+  string[9] = 'r';
+  string[10] = 'l';
+  string[11] = 'd';
+  string[12] = '!';
+  string[13] = '\0';
 };
--- src/parallel_version.hpp
+++ src/parallel_version.hpp
@@ -4,6 +4,8 @@
 #include<vector>
 #include<iostream>
 
+#define MEM_SIZE (128)
+#define MAX_SOURCE_SIZE (4096)
 
 void tryInitOpenCL() {
     cl_int err;
@@ -13,67 +15,65 @@ void tryInitOpenCL() {
         std::string message("Error calling clGetPlatformIDs. Error code: ");
         throw std::runtime_error(message + std::to_string(err));
     }
-
     printf("\nDetected OpenCL platforms: %d \n", numPlatforms);
 }
 
-// returns the opencl code in a specific file as a string
-std::string readClFile(std::string filename) {
-    std::ifstream file(filename);
-    return std::string(std::istreambuf_iterator<char>(file), (std::istreambuf_iterator<char>()));
+// loads kernel source code into a buffer and returns the size of the source string
+size_t readClFile(const char* filename, char* buffer) {
+    auto file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+
+    buffer = (char*)malloc(MAX_SOURCE_SIZE);
+    auto source_size = sizeof(buffer) / sizeof(buffer[0]);
+    fclose(file);
+    return source_size;
 }
-
-cl_platform_id getPlatformId(cl_int* return_code) {
-    cl_platform_id platform_id;
-    cl_uint num_platforms;
-    *return_code = clGetPlatformIDs(1, &platform_id, &num_platforms);
-    return platform_id;
-}
-
-
-
 
 void run_parallel() {
     try {
         tryInitOpenCL();
     }
     catch (const std::exception& e) {
+        std::cout << "-1" << std::endl;
         std::cerr << e.what() << '\n';
         return;
     }
+
     cl_int ret;
-    auto platform = getPlatformId(&ret);
+    cl_platform_id platform_id;
+    cl_uint num_platforms;
+    ret = clGetPlatformIDs(1, &platform_id, &num_platforms);
 
     cl_device_id device_id;
     cl_uint num_devices;
-    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices);
+    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &num_devices);
 
+    char message[MEM_SIZE];
     auto context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 
     auto command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
-    auto memory = clCreateBuffer(context, CL_MEM_READ_WRITE, 32 * sizeof(char), NULL, &ret);
+    auto memory = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(char), NULL, &ret);
 
-    /* Create Kernel Program from the source */
-    auto cl_code = readClFile("parallel.cl");
-    auto program = clCreateProgramWithSource(context, 1, (const char**)&cl_code, (const size_t*)cl_code.length() + 1, &ret);
+    char* kernel_source;
+    auto source_size = readClFile("src/parallel.cl", kernel_source);
+    auto program = clCreateProgramWithSource(context, 1, (const char**)&kernel_source, &source_size, &ret);
 
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
-    auto kernel = clCreateKernel(program, "test", &ret);
+    auto kernel = clCreateKernel(program, "hello_world", &ret);
 
-    /* Set OpenCL Kernel Parameters */
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&memory);
 
-    /* Execute OpenCL Kernel */
     ret = clEnqueueTask(command_queue, kernel, 0, NULL, NULL);
 
-    /* Copy results from the memory buffer */
     ret = clEnqueueReadBuffer(command_queue, memory, CL_TRUE, 0,
-        32 * sizeof(char), string, 0, NULL, NULL);
+        MEM_SIZE * sizeof(char), message, 0, NULL, NULL);
 
-    /* Display Result */
-    puts(string);
+    puts(message);
 
     /* Finalization */
     ret = clFlush(command_queue);
@@ -83,7 +83,5 @@ void run_parallel() {
     ret = clReleaseMemObject(memory);
     ret = clReleaseCommandQueue(command_queue);
     ret = clReleaseContext(context);
-
-    free(source_str);
 }
 
