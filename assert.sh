ac2c158ffd0131139adcfe1ebf995dba304f9b24
Christian Lascsak <christian.lascsak7@gmail.com>
2022-01-22 17:17:27 +0100

    update assert script and add test task

--- assert.sh
+++ assert.sh
@@ -1,4 +1,4 @@
 #usr/bin/bash
 echo "Asserting Sequential version..."
-difference="$(diff sequential_matrix.txt matrix_original.txt)"
+difference="$(diff sequential_matrix.txt matrix_parallel.txt)"
 [[ ! -z "$difference" ]] && echo "NOK" || echo "OK"
