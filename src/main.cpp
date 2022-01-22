f231fa354d6c864b3867d60ea14561c79ff16d31
Christian Lascsak <christian.lascsak7@gmail.com>
2022-01-22 22:13:03 +0100

    print speedup and readd safeimage function

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char const* argv[]) {
     auto parallel_time = run_parallel();
     printf("sequential execution took: %f \n", seq_time);
     printf("parallel execution took: %f \n", parallel_time);
+    printf("speedup: %f \n", seq_time / parallel_time);
     return 0;
 }
--- src/parallel_version.hpp
+++ src/parallel_version.hpp
@@ -4,6 +4,21 @@
 #include<vector>
 #include<iostream>
 
+void safeImage(const char* filename, float* A, const size_t m, size_t n, size_t p) {
+    auto index_at = [&m, &n, &p](int x, int y, int z) {return x + m * (y + n * z);};
+    std::ofstream ofs(filename, std::ofstream::out);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < p; k++) {
+                ofs << " " << A[index_at(i, j, k)];
+            }
+            ofs << "    ";
+        }
+        ofs << std::endl;
+    }
+    ofs.close();
+}
+
 // loads kernel source code into a buffer and returns the size of the source string
 static const char* source[] = {
 "int index_at(int x, int y, int z) {\n"
